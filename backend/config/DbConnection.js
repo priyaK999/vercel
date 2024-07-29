9555ac1327025bc951fefb9b492ea259ace793da
priyaK9157 <priyakumari2003004@gmail.com>
2024-07-29 08:47:32 +0530

    Initial commit

--- backend/config/DbConnection.js
+++ backend/config/DbConnection.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 require('dotenv').config(); // Ensure dotenv is loaded
 
 const dbConnect = () => {
-  mongoose.connect(process.env.Mongo_Url,{
+  mongoose.connect(process.env.mongo_url,{
    
   })
     .then(() => {
       console.log('DB connection successful');
-    })
+    }) 
     .catch((err) => {
       console.error('Error in connecting DB:', err);
       process.exit(1);
